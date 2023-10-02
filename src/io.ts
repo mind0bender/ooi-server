4dc8de4e077f6ed6e716de6d27592f91a9f7a074
mind0bender <mind0bender@gmail.com>
2023-10-02 17:31:21 +0530

    { "CREATE": ["create a room", "check room exists status"] }

--- src/io.ts
+++ src/io.ts
@@ -11,9 +11,31 @@ export const io = new Server(server, {
 });
 
 io.on("connection", (user: Socket) => {
-  console.log(`++ user: ${ansiColors.greenBright(user.id)}`);
+  console.log(
+    `++ user: ${ansiColors.greenBright(user.id)} | ${io.sockets.sockets.size}`
+  );
   user.emit("connected");
+
+  user.on("create_room", (roomId: string) => {
+    if (io.sockets.adapter.rooms.has(roomId)) {
+      user.emit("room_exists", roomId);
+      return;
+    }
+    user.join(roomId);
+    user.emit("room_created", roomId);
+  });
+
+  user.on("check_room_exists", (roomId: string) => {
+    if (io.sockets.adapter.rooms.has(roomId)) {
+      user.emit("check_room_exists_res", { roomId, exists: true });
+      return;
+    }
+    user.emit("check_room_exists_res", { roomId, exists: false });
+  });
+
   user.on("disconnect", () => {
-    console.log(`-- user: ${ansiColors.red(user.id)}`);
+    console.log(
+      `-- user: ${ansiColors.red(user.id)} | ${io.sockets.sockets.size}`
+    );
   });
 });
